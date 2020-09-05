43209d650d37003bfa9d35948466f9e77d4d12d2
Alabbey2 <47299377+Alabbey2@users.noreply.github.com>
2020-09-05 18:32:14 +0300

    2nd commit modified all files

---
+++ script.js
@@ -0,0 +1,15 @@
+var greetings = "good morning";
+
+newGreetings = (greetings);
+
+console.log(newGreetings);
+
+var x = document.getElementById("greet");
+x.style.backgroundColor = "green";
+
+var el = document.createElement("p");
+el.innerHTML ="lorem20";
+document.body.appendChild(el);
+
+el.style.backgroundColor = "red";
+
